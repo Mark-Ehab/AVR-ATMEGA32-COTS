310a60d70aacd1025c160fde272a35145a2763c1
Mark Ehab <markehabtawfiq@gmail.com>
2023-08-17 15:41:52 +0300

    Update DIO_Program.c and DIO_Interface.h files in DIO driver

--- 02-MCAL/02-DIO/DIO_Interface.h
+++ 02-MCAL/02-DIO/DIO_Interface.h
@@ -16,37 +16,37 @@
 /*-----------------------------------------------------------------------------------*/
 
 /* DIO Ports */
-#define PORT_A 			(u8)0
-#define PORT_B 			(u8)1
-#define PORT_C 			(u8)2
-#define PORT_D 			(u8)3
+#define DIO_PORT_A 			0U
+#define DIO_PORT_B 			1U
+#define DIO_PORT_C 			2U
+#define DIO_PORT_D 			3U
 
 
 /* DIO Pins */
-#define PIN_0 			(u8)0
-#define PIN_1 			(u8)1
-#define PIN_2 			(u8)2
-#define PIN_3 			(u8)3
-#define PIN_4 			(u8)4
-#define PIN_5 			(u8)5
-#define PIN_6 			(u8)6
-#define PIN_7 			(u8)7
+#define DIO_PIN_0 			0U
+#define DIO_PIN_1 			1U
+#define DIO_PIN_2 			2U
+#define DIO_PIN_3 			3U
+#define DIO_PIN_4 			4U
+#define DIO_PIN_5 			5U
+#define DIO_PIN_6 			6U
+#define DIO_PIN_7 			7U
 
 
 /* Pin Level Signals */
-#define INPUT 			(u8)0
-#define OUTPUT			(u8)1
+#define DIO_PIN_INPUT 			0U
+#define DIO_PIN_OUTPUT			1U
 
-#define LOW 			(u8)0
-#define HIGH 			(u8)1
+#define DIO_PIN_LOW 			0U
+#define DIO_PIN_HIGH 			1U
 
 
 /* Port Level Signals */
-#define FULL_INPUT 		0x00
-#define FULL_OUTPUT		0xff
+#define DIO_PORT_INPUT 		        0x00
+#define DIO_PORT_OUTPUT		        0xff
 
-#define FULL_LOW 		0x00
-#define FULL_HIGH 		0xff
+#define DIO_PORT_LOW 		        0x00
+#define DIO_PORT_HIGH 		        0xff
 
 
 /*-----------------------------------------------------------------------------------*/
@@ -59,48 +59,48 @@
 
 /**********************************************************************************/
 /* Description     : Set port direction (Input or Output)			  */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8dir			          */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Dir			          */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8setPortDir(u8 Copy_u8port , u8 Copy_u8dir);
+u8 DIO_u8setPortDir(u8 Copy_u8Port , u8 Copy_u8Dir);
 /**********************************************************************************/
 /* Description     : Set port value (High  or Low)				  */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8dval			          */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Val			          */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8setPortVal(u8 Copy_u8port , u8 Copy_u8dval);
+u8 DIO_u8setPortVal(u8 Copy_u8Port , u8 Copy_u8Val);
 /**********************************************************************************/
 /* Description     : Get the value on port 	     				  */
-/* Input Arguments : u8 Copy_u8port , u8* Copy_u8pval			          */
+/* Input Arguments : u8 Copy_u8Port , u8* Copy_pu8Val			          */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8getPortVal(u8 Copy_u8port , u8* Copy_u8pval);
+u8 DIO_u8getPortVal(u8 Copy_u8Port , u8* Copy_pu8Val);
 
 /* Pin Level */
 
 /**********************************************************************************/
 /* Description     : Set pin direction (Input  or Output)			  */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8dir		  */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Dir		  */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8setPinDir(u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8dir);
+u8 DIO_u8setPinDir(u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Dir);
 /**********************************************************************************/
 /* Description     : Set pin value (High or Low)			          */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8val		  */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Val		  */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8setPinVal(u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8val);
+u8 DIO_u8setPinVal(u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Val);
 /**********************************************************************************/
 /* Description     : Get pin value					          */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8pin , u8* Copy_u8pval		  */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Pin , u8* Copy_pu8Val		  */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8getPinVal(u8 Copy_u8port , u8 Copy_u8pin , u8* Copy_u8pval);
+u8 DIO_u8getPinVal(u8 Copy_u8Port , u8 Copy_u8Pin , u8* Copy_pu8Val);
 /**********************************************************************************/
 /* Description     : Toggle current pin value					  */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8pin			          */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Pin			          */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8togglePinVal(u8 Copy_u8port , u8 Copy_u8pin);
+u8 DIO_u8togglePinVal(u8 Copy_u8Port , u8 Copy_u8Pin);
 
 #endif /* MCAL_DIO_DIO_INTERFACE_H_  */
--- 02-MCAL/02-DIO/DIO_Program.c
+++ 02-MCAL/02-DIO/DIO_Program.c
@@ -23,25 +23,25 @@
 
 /**********************************************************************************/
 /* Description     : Set port direction (Input or Output)			  */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8dir			          */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Dir			          */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8setPortDir(u8 Copy_u8port , u8 Copy_u8dir)
+u8 DIO_u8setPortDir(u8 Copy_u8Port , u8 Copy_u8Dir)
 {
 	u8 local_u8errStatus = RT_OK ;
-	switch(Copy_u8port)
+	switch(Copy_u8Port)
 	{
-	case PORT_A:
-		DDRA=Copy_u8dir;
+	case DIO_PORT_A:
+		DDRA=Copy_u8Dir;
 		break;
-	case PORT_B:
-		DDRB=Copy_u8dir;
+	case DIO_PORT_B:
+		DDRB=Copy_u8Dir;
 		break;
-	case PORT_C:
-		DDRC=Copy_u8dir;
+	case DIO_PORT_C:
+		DDRC=Copy_u8Dir;
 		break;
-	case PORT_D:
-		DDRD=Copy_u8dir;
+	case DIO_PORT_D:
+		DDRD=Copy_u8Dir;
 		break;
 	default:
 		local_u8errStatus = RT_NOK ;
@@ -50,25 +50,25 @@ u8 DIO_u8setPortDir(u8 Copy_u8port , u8 Copy_u8dir)
 }
 /**********************************************************************************/
 /* Description     : Set port value (High  or Low)				  */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8dval			          */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Val			          */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8setPortVal(u8 Copy_u8port , u8 Copy_u8dval)
+u8 DIO_u8setPortVal(u8 Copy_u8Port , u8 Copy_u8Val)
 {
 	u8 local_u8errStatus = RT_OK ;
-	switch(Copy_u8port)
+	switch(Copy_u8Port)
 	{
-	case PORT_A:
-		PORTA=Copy_u8dval;
+	case DIO_PORT_A:
+		PORTA=Copy_u8Val;
 		break;
-	case PORT_B:
-		PORTB=Copy_u8dval;
+	case DIO_PORT_B:
+		PORTB=Copy_u8Val;
 		break;
-	case PORT_C:
-		PORTC=Copy_u8dval;
+	case DIO_PORT_C:
+		PORTC=Copy_u8Val;
 		break;
-	case PORT_D:
-		PORTD=Copy_u8dval;
+	case DIO_PORT_D:
+		PORTD=Copy_u8Val;
 		break;
 	default:
 		local_u8errStatus = RT_NOK ;
@@ -77,25 +77,25 @@ u8 DIO_u8setPortVal(u8 Copy_u8port , u8 Copy_u8dval)
 }
 /**********************************************************************************/
 /* Description     : Get the value on port 	     				  */
-/* Input Arguments : u8 Copy_u8port , u8* Copy_u8pval			          */
+/* Input Arguments : u8 Copy_u8Port , u8* Copy_pu8Val			          */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8getPortVal(u8 Copy_u8port , u8* Copy_u8pval)
+u8 DIO_u8getPortVal(u8 Copy_u8Port , u8* Copy_pu8Val)
 {
 	u8 local_u8errStatus = RT_OK ;
-	switch(Copy_u8port)
+	switch(Copy_u8Port)
 	{
-	case PORT_A:
-		*Copy_u8pval=PINA;
+	case DIO_PORT_A:
+		*Copy_pu8Val=PINA;
 		break;
-	case PORT_B:
-		*Copy_u8pval=PINB;
+	case DIO_PORT_B:
+		*Copy_pu8Val=PINB;
 		break;
-	case PORT_C:
-		*Copy_u8pval=PINC;
+	case DIO_PORT_C:
+		*Copy_pu8Val=PINC;
 		break;
-	case PORT_D:
-		*Copy_u8pval=PIND;
+	case DIO_PORT_D:
+		*Copy_pu8Val=PIND;
 		break;
 	default:
 		local_u8errStatus = RT_NOK ;
@@ -107,49 +107,49 @@ u8 DIO_u8getPortVal(u8 Copy_u8port , u8* Copy_u8pval)
 
 /**********************************************************************************/
 /* Description     : Set pin direction (Input  or Output)			  */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8dir		  */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Dir		  */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8setPinDir(u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8dir)
+u8 DIO_u8setPinDir(u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Dir)
 {
 	u8 local_u8errStatus = RT_OK ;
-	if(Copy_u8pin >= PIN_0 && Copy_u8pin <= PIN_7 )
+	if(Copy_u8Pin >= DIO_PIN_0 && Copy_u8Pin <= DIO_PIN_7 )
 	{
-		if(Copy_u8dir == INPUT)
+		if(Copy_u8Dir == DIO_PIN_INPUT)
 		{
-			switch(Copy_u8port)
+			switch(Copy_u8Port)
 			{
-			case PORT_A:
-				CLR_BIT(DDRA,Copy_u8pin);
+			case DIO_PORT_A:
+				CLR_BIT(DDRA,Copy_u8Pin);
 				break;
-			case PORT_B:
-				CLR_BIT(DDRB,Copy_u8pin);
+			case DIO_PORT_B:
+				CLR_BIT(DDRB,Copy_u8Pin);
 				break;
-			case PORT_C:
-				CLR_BIT(DDRC,Copy_u8pin);
+			case DIO_PORT_C:
+				CLR_BIT(DDRC,Copy_u8Pin);
 				break;
-			case PORT_D:
-				CLR_BIT(DDRD,Copy_u8pin);
+			case DIO_PORT_D:
+				CLR_BIT(DDRD,Copy_u8Pin);
 				break;
 			default:
 				local_u8errStatus = RT_NOK ;
 			}
 		}
-		else if(Copy_u8dir == OUTPUT)
+		else if(Copy_u8Dir == DIO_PIN_OUTPUT)
 		{
-			switch(Copy_u8port)
+			switch(Copy_u8Port)
 			{
-			case PORT_A:
-				SET_BIT(DDRA,Copy_u8pin);
+			case DIO_PORT_A:
+				SET_BIT(DDRA,Copy_u8Pin);
 				break;
-			case PORT_B:
-				SET_BIT(DDRB,Copy_u8pin);
+			case DIO_PORT_B:
+				SET_BIT(DDRB,Copy_u8Pin);
 				break;
-			case PORT_C:
-				SET_BIT(DDRC,Copy_u8pin);
+			case DIO_PORT_C:
+				SET_BIT(DDRC,Copy_u8Pin);
 				break;
-			case PORT_D:
-				SET_BIT(DDRD,Copy_u8pin);
+			case DIO_PORT_D:
+				SET_BIT(DDRD,Copy_u8Pin);
 				break;
 			default:
 				local_u8errStatus = RT_NOK ;
@@ -168,49 +168,49 @@ u8 DIO_u8setPinDir(u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8dir)
 }
 /**********************************************************************************/
 /* Description     : Set pin value (High or Low)			          */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8val		  */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Val		  */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8setPinVal(u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8val)
+u8 DIO_u8setPinVal(u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Val)
 {
 	u8 local_u8errStatus = RT_OK ;
-	if(Copy_u8pin >= PIN_0 && Copy_u8pin <= PIN_7 )
+	if(Copy_u8Pin >= DIO_PIN_0 && Copy_u8Pin <= DIO_PIN_7 )
 	{
-		if(Copy_u8val == LOW)
+		if(Copy_u8Val == DIO_PIN_LOW)
 		{
-			switch(Copy_u8port)
+			switch(Copy_u8Port)
 			{
-			case PORT_A:
-				CLR_BIT(PORTA,Copy_u8pin);
+			case DIO_PORT_A:
+				CLR_BIT(PORTA,Copy_u8Pin);
 				break;
-			case PORT_B:
-				CLR_BIT(PORTB,Copy_u8pin);
+			case DIO_PORT_B:
+				CLR_BIT(PORTB,Copy_u8Pin);
 				break;
-			case PORT_C:
-				CLR_BIT(PORTC,Copy_u8pin);
+			case DIO_PORT_C:
+				CLR_BIT(PORTC,Copy_u8Pin);
 				break;
-			case PORT_D:
-				CLR_BIT(PORTD,Copy_u8pin);
+			case DIO_PORT_D:
+				CLR_BIT(PORTD,Copy_u8Pin);
 				break;
 			default:
 				local_u8errStatus = RT_NOK ;
 			}
 		}
-		else if(Copy_u8val == HIGH)
+		else if(Copy_u8Val == DIO_PIN_HIGH)
 		{
-			switch(Copy_u8port)
+			switch(Copy_u8Port)
 			{
-			case PORT_A:
-				SET_BIT(PORTA,Copy_u8pin);
+			case DIO_PORT_A:
+				SET_BIT(PORTA,Copy_u8Pin);
 				break;
-			case PORT_B:
-				SET_BIT(PORTB,Copy_u8pin);
+			case DIO_PORT_B:
+				SET_BIT(PORTB,Copy_u8Pin);
 				break;
-			case PORT_C:
-				SET_BIT(PORTC,Copy_u8pin);
+			case DIO_PORT_C:
+				SET_BIT(PORTC,Copy_u8Pin);
 				break;
-			case PORT_D:
-				SET_BIT(PORTD,Copy_u8pin);
+			case DIO_PORT_D:
+				SET_BIT(PORTD,Copy_u8Pin);
 				break;
 			default:
 				local_u8errStatus = RT_NOK ;
@@ -229,27 +229,27 @@ u8 DIO_u8setPinVal(u8 Copy_u8port , u8 Copy_u8pin , u8 Copy_u8val)
 }
 /**********************************************************************************/
 /* Description     : Get pin value					          */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8pin , u8* Copy_u8pval		  */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Pin , u8* Copy_pu8Val		  */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8getPinVal(u8 Copy_u8port , u8 Copy_u8pin , u8* Copy_u8pval)
+u8 DIO_u8getPinVal(u8 Copy_u8Port , u8 Copy_u8Pin , u8* Copy_pu8Val)
 {
 	u8 local_u8errStatus = RT_OK ;
-	if(Copy_u8pin >= PIN_0 && Copy_u8pin <= PIN_7 )
+	if(Copy_u8Pin >= DIO_PIN_0 && Copy_u8Pin <= DIO_PIN_7 )
 	{
-		switch(Copy_u8port)
+		switch(Copy_u8Port)
 		{
-		case PORT_A:
-			*Copy_u8pval=GET_BIT(PINA,Copy_u8pin);
+		case DIO_PORT_A:
+			*Copy_pu8Val=GET_BIT(PINA,Copy_u8Pin);
 			break;
-		case PORT_B:
-			*Copy_u8pval=GET_BIT(PINB,Copy_u8pin);
+		case DIO_PORT_B:
+			*Copy_pu8Val=GET_BIT(PINB,Copy_u8Pin);
 			break;
-		case PORT_C:
-			*Copy_u8pval=GET_BIT(PINC,Copy_u8pin);
+		case DIO_PORT_C:
+			*Copy_pu8Val=GET_BIT(PINC,Copy_u8Pin);
 			break;
-		case PORT_D:
-			*Copy_u8pval=GET_BIT(PIND,Copy_u8pin);
+		case DIO_PORT_D:
+			*Copy_pu8Val=GET_BIT(PIND,Copy_u8Pin);
 			break;
 		default:
 			local_u8errStatus = RT_NOK ;
@@ -263,27 +263,27 @@ u8 DIO_u8getPinVal(u8 Copy_u8port , u8 Copy_u8pin , u8* Copy_u8pval)
 }
 /**********************************************************************************/
 /* Description     : Toggle current pin value					  */
-/* Input Arguments : u8 Copy_u8port , u8 Copy_u8pin			          */
+/* Input Arguments : u8 Copy_u8Port , u8 Copy_u8Pin			          */
 /* Return          : u8                       				          */
 /**********************************************************************************/
-u8 DIO_u8togglePinVal(u8 Copy_u8port , u8 Copy_u8pin)
+u8 DIO_u8togglePinVal(u8 Copy_u8Port , u8 Copy_u8Pin)
 {
 	u8 local_u8errStatus = RT_OK;
-	if(Copy_u8pin >= PIN_0 && Copy_u8pin <= PIN_7 )
+	if(Copy_u8Pin >= DIO_PIN_0 && Copy_u8Pin <= DIO_PIN_7 )
 	{
-		switch(Copy_u8port)
+		switch(Copy_u8Port)
 		{
-		case PORT_A:
-			TOGGLE_BIT(PORTA,Copy_u8pin);
+		case DIO_PORT_A:
+			TOGGLE_BIT(PORTA,Copy_u8Pin);
 			break;
-		case PORT_B:
-			TOGGLE_BIT(PORTB,Copy_u8pin);
+		case DIO_PORT_B:
+			TOGGLE_BIT(PORTB,Copy_u8Pin);
 			break;
-		case PORT_C:
-			TOGGLE_BIT(PORTC,Copy_u8pin);
+		case DIO_PORT_C:
+			TOGGLE_BIT(PORTC,Copy_u8Pin);
 			break;
-		case PORT_D:
-			TOGGLE_BIT(PORTD,Copy_u8pin);
+		case DIO_PORT_D:
+			TOGGLE_BIT(PORTD,Copy_u8Pin);
 			break;
 		default:
 			local_u8errStatus = RT_NOK ;
